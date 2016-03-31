97202a67a74464ab5b78b2a6d83655264070bc9a
Oradruir <oradruir@hotmail.com>
2016-03-30 22:30:25 -0400

    Préparation du fichier header.php qui sera appellé sur toutes les pages

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Titre</title>
+    </head>
+    <body>
+		<div id="conteneur">
+			<header>
+				<nav>
+				
+				</nav>
+			</header>
